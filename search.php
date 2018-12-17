0eec711482536015a4e8f08c5c5ad03958e67833
Daniel Scheer <danielscheer@live.nl>
2018-12-17 11:24:33 +0100

    added addProgram with form to query

--- search.php
+++ search.php
@@ -15,7 +15,7 @@
     ?>
      <div class="row">
         <div class="col-sm academic-requirements-top">
-            <a href="search.php?program=Media"><i class="fas fa-graduation-cap"></i> See by academic requirement</a>
+            <p onclick="addProgram('Media')"><i class="fas fa-graduation-cap"></i> See by academic requirement</p>
         </div>
     </div>
     <div class="row">
@@ -237,6 +237,11 @@
 
         function addCategory(cat) {
             $('#categoryHidden').attr("value", cat);
+            $('#hiddenForm').submit();
+        }
+
+        function addProgram(pro) {
+            $('#programHidden').attr("value", pro);
             $('#hiddenForm').submit();
         }
     </script>
